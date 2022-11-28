351fc4d16c6ef22e7cb241d748b7e69951056345
Saket Kumar <kumar.sak@northeastern.edu>
2022-11-27 22:12:22 -0500

    Removed empty line

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -16,7 +16,6 @@ const findTuits = async (req, res) => {
 
 const createTuit = async (req, res) => {
   const newTuit = req.body;
-
   newTuit.likes = 0;
   newTuit.liked = false;
   newTuit.dislikes = 0;
